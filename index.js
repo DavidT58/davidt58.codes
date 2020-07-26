b8ea0f5cc3377edb44b6e3df22ef029ac5403644
DavidT58 <cd180700d@student.etf.bg.ac.rs>
2020-07-26 17:38:40 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+const port = 80;
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'static/index.html'));
+});
