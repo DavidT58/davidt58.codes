17592e36d90d7a9f84864d2bc8a9aebbb64de371
DavidT58 <cd180700d@student.etf.bg.ac.rs>
2020-08-16 13:28:30 +0200

    test db

--- server/index.js
+++ server/index.js
@@ -2,6 +2,15 @@ const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
 const helmet = require('helmet');
+const yup = require('yup');
+const monk = require('monk');
+const { nanoid } = require('nanoid');
+
+require('dotenv').config();
+
+const db = monk(process.env.MONGO_URI);
+const urls = db.get('urls');
+urls.createIndex('name');
 
 const app = express();
 
@@ -19,9 +28,50 @@ app.get('/:id', (req, res) => {
     //TODO: redirect to url
 });
 
-app.post('/url', (req, res) => {
-    //TODO: create a short url
+const schema = yup.object().shape({
+    slug: yup.string().trim().matches(/[\w\-]/i),
+    url: yup.string().trim().url().required(),
+})
+
+app.post('/url', async (req, res, next) => {
+    let { slug, url } = req.body;
+    try{
+        await schema.validate({
+            slug,
+            url,
+        });
+        if(!slug){
+            slug = nanoid(5);
+        } else{
+            const existing = await urls.findOne({ slug });
+            if(existing){
+                throw new Error('Slug in use');
+            }
+        }
+        slug = slug.toLowerCase();
+        const newUrl = {
+            url,
+            slug,
+        }
+        const created = await urls.insert(newUrl);
+        res.json(created);
+    } catch (error){
+        next(error);
+    }
 });
+
+app.use((error, req, res, next) => {
+    if(error.status){
+        res.status(error.status);
+    }
+    else{
+        res.status(500);
+    }
+    res.json({
+        message: error.message,
+        
+    })
+})
 
 const port = 8000;
 
